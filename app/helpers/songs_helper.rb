90a9791ea22c8413169dcf4e8200f136f60c58c2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-10-01 22:30:12 +0000

    Done.

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,10 @@
 module SongsHelper
-
+  def display_artist(song)
+    artist = song.artist
+    if artist != nil
+      link_to artist.name, artist_path(artist)
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
 end
